9ea762b731b8c255dfdf82d2763e89cf59b60d61
yazpur1 <yan.azpur.27@unsch.edu.pe>
2024-09-02 23:06:33 -0500

    primera subida

---
+++ src/main/java/iadataperu/com/hospital_app/HospitalAppApplication.java
@@ -0,0 +1,14 @@
+package iadataperu.com.hospital_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalAppApplication.class, args);
+		System.out.println("Hola Mundo");
+	}
+
+}
